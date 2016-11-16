5cc17c57627c3b1f9a079d22b413274eb211ef70
mkassner <moritzkassner@gmail.com>
2016-11-16 09:44:35 +0100

    Update hmd_calibration_client.py

--- hmd_calibration/hmd_calibration_client.py
+++ hmd_calibration/hmd_calibration_client.py
@@ -22,10 +22,9 @@ def get_pupil_timestamp():
     req.send('t') #see Pupil Remote Plugin for details
     return float(req.recv())
 
-# set calibration method to hmd calibration
+# set start eye windows
 n = {'subject':'eye_process.should_start.0','eye_id':0, 'args':{}}
 print send_recv_notification(n)
-# set calibration method to hmd calibration
 n = {'subject':'eye_process.should_start.1','eye_id':1, 'args':{}}
 print send_recv_notification(n)
 time.sleep(2)
