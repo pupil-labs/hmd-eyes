91d7f67592d29cf6b369c123ae3b564a3c7b6486
Moritz Kassner <moritzkassner@gmail.com>
2017-03-09 13:56:11 +0100

    update to python3. TODO update hmd_calibration.py as well.

--- hmd_calibration/notification_monitor.py
+++ hmd_calibration/notification_monitor.py
@@ -5,11 +5,11 @@ ctx = zmq.Context()
 requester = ctx.socket(zmq.REQ)
 requester.connect('tcp://localhost:50020')
 
-requester.send('SUB_PORT')
-ipc_sub_port = requester.recv()
+requester.send_string('SUB_PORT')
+ipc_sub_port = requester.recv_string()
 monitor = Msg_Receiver(ctx,'tcp://localhost:%s'%ipc_sub_port,topics=('notify.',))
 
 while True:
-    print monitor.recv()
+    print(monitor.recv())
 
 
