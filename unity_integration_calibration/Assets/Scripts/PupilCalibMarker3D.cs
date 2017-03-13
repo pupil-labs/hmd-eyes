b962395c7acdb8c2dc6f0395d2e0175444fd8f50
kornellvarga <kornel.l.varga@gmail.com>
2017-03-13 13:49:21 +0100

    Visuals for 3D Calib Marker
    If you want to take a look> open the PupilGaze>>Calibration>>3D and
    play. there is no 3D calibration methos attached to the visuals yet, but
    you can test how it looks by pressing the "A" button on the keyboard for
    now. Every press represents one step in the samples of the calibration.

--- unity_integration_calibration/Assets/Scripts/PupilCalibMarker3D.cs
+++ unity_integration_calibration/Assets/Scripts/PupilCalibMarker3D.cs
@@ -4,10 +4,77 @@ using UnityEngine;
 
 public class PupilCalibMarker3D : MonoBehaviour {
 
-	public float animLength;
+	public float animTotalLength;
+	public int animSegmentAmount;
+
+	[Range(0.01f,5.0f)]
+	public float pulsateSpeed = 1f;
+	[Range(0.1f,1.0f)]
+	public float maxScale;
+	[Range(0.1f,1.0f)]
+	public float minScale;
+
+	public Color StartColor;
+	public Color EndColor;
+
+	public Sprite sprite;
+
+	private float animSegmentLenght;
+	private SpriteRenderer haloRenderer;
+	public int tmp = 0;
+
+	void Awake(){
+		animSegmentLenght = animTotalLength / animSegmentAmount;
+		haloRenderer = transform.FindChild ("PupilLabsLogoHalo").gameObject.GetComponent<SpriteRenderer> ();
+	}
 
 	void Update(){
-	
+		PulsateHalo ();
+		if (Input.GetKeyUp (KeyCode.A)) {
+			tmp++;
+			UpdateAnim(tmp);
+		}
+	}
+
+	void PulsateHalo(){
+		float _t = Time.time;
+		Color _c = new Color (1, 1, 1, Mathf.Abs (Mathf.Sin (Time.time * (pulsateSpeed + Time.deltaTime))));
+
+		haloRenderer.color = _c;
+	}
+
+	public void UpdateAnim(int index){
+
+		float _p = Mathf.InverseLerp (0.9f, animSegmentAmount, index);
+		//print ("Segment percentage : " + _p);
+		float _s = Mathf.Lerp (minScale, maxScale, _p);
+		//print ("Segment scale : " + _s);
+
+		GameObject _go = new GameObject ();
+		_go.transform.parent = this.gameObject.transform;
+		_go.transform.localScale = new Vector3 (0, 0, 0);
+		SpriteRenderer _sRenderer = _go.AddComponent<SpriteRenderer> ();
+		_sRenderer.sprite = sprite;
+		_sRenderer.color = Color.Lerp (StartColor, EndColor, _p);
+
+
+		StartCoroutine (SegmentAnimation (animSegmentLenght, _s, _go));
+
+	}
+
+	IEnumerator SegmentAnimation(float length, float scale, GameObject _go){
+		
+		float currTime = 0f;
+		float _minS = 0.1f;
+		float _maxS = scale;
+		float step = scale / length;
+		while (currTime<length) {
+			_go.transform.localScale += new Vector3 (step, step, step);
+			currTime++;
+			yield return null;
+		}
+			
+
 	}
 
 }
