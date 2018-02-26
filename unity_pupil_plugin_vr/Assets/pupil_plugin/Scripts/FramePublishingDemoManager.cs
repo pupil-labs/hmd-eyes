121c243ec9f3391b2b959685a3c6d65dd5717b4f
André Nicolai <AndreNicolai@live.de>
2018-02-26 10:09:16 +0100

    Fixed error with Frame Publishing demo scene

---
+++ unity_pupil_plugin_vr/Assets/pupil_plugin/Scripts/FramePublishingDemoManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramePublishingDemoManager : MonoBehaviour 
+{
+	FramePublishing publisher;
+
+	void OnEnable () 
+	{
+		PupilTools.OnConnected += StartFramePublishing;
+		PupilTools.OnDisconnecting += StopFramePublishing;
+	}
+
+	void StartFramePublishing()
+	{
+		if (publisher == null)
+			publisher = gameObject.AddComponent<FramePublishing> ();
+		else
+			publisher.enabled = true;
+	}
+
+	void StopFramePublishing()
+	{
+		if (publisher != null)
+			publisher.enabled = false;
+	}
+
+	void OnDisable()
+	{
+		PupilTools.OnConnected -= StartFramePublishing;
+		PupilTools.OnDisconnecting -= StopFramePublishing;
+	}
+}
