22e76d39a0e39cc537f8ea167a1cb10ab6a12a9d
Felix Lange <langefx@gmail.com>
2019-02-08 17:33:14 +0100

    remove unused dependencies

--- plugin/Scripts/Stream.cs
+++ plugin/Scripts/Stream.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NetMQ;
 using NetMQ.Sockets;
 using MessagePack;
 
