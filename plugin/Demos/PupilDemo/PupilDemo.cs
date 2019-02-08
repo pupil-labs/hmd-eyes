0c2ca1e45c6d0b3e71d16d40aff0174fb194ca65
Felix Lange <langefx@gmail.com>
2019-02-08 16:29:38 +0100

    more copied helpers needed for the migrated PupilDemo

---
+++ plugin/Demos/PupilDemo/PupilDemo.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs.Demos
+{
+
+    public class PupilDemo : MonoBehaviour
+    {
+        public PupilLabs.Stream stream;
+
+        void OnEnable()
+        {
+            stream.OnConnected += StartPupilSubscription;
+            stream.OnDisconnecting += StopPupilSubscription;
+
+            if (stream.IsConnected)
+            {
+                StartPupilSubscription();
+            }
+        }
+
+        void OnDisable()
+        {
+            stream.OnConnected -= StartPupilSubscription;
+            stream.OnDisconnecting -= StopPupilSubscription;
+
+            if (stream.IsConnected)
+            {
+                StopPupilSubscription();
+            }
+        }
+
+        void StartPupilSubscription()
+        {
+
+            Debug.Log("StartPupilSubscription");
+
+            stream.InitializeSubscriptionSocket("pupil.", CustomReceiveData);
+        }
+
+        void StopPupilSubscription()
+        {
+
+            Debug.Log("StopPupilSubscription");
+
+            stream.CloseSubscriptionSocket("pupil.", CustomReceiveData);
+        }
+
+        void CustomReceiveData(string topic, Dictionary<string, object> dictionary, byte[] thirdFrame = null)
+        {
+
+            Debug.Log($"Topic {topic} received");
+            foreach (var item in dictionary)
+            {
+                switch (item.Key)
+                {
+                    case "topic":
+                    case "method":
+                    case "id":
+                        var textForKey = PupilLabs.Helpers.StringFromDictionary(dictionary, item.Key);
+                        // Do stuff
+                        break;
+                    case "confidence":
+                    case "timestamp":
+                    case "diameter":
+                        var valueForKey = PupilLabs.Helpers.FloatFromDictionary(dictionary, item.Key);
+                        // Do stuff
+                        break;
+                    case "norm_pos":
+                        var positionForKey = PupilLabs.Helpers.VectorFromDictionary(dictionary, item.Key);
+                        // Do stuff
+                        break;
+                    case "ellipse":
+                        var dictionaryForKey = PupilLabs.Helpers.DictionaryFromDictionary(dictionary, item.Key);
+                        foreach (var pupilEllipse in dictionaryForKey)
+                        {
+                            switch (pupilEllipse.Key.ToString())
+                            {
+                                case "angle":
+                                    var angle = (float)(double)pupilEllipse.Value;
+                                    // Do stuff
+                                    break;
+                                case "center":
+                                case "axes":
+                                    var vector = PupilLabs.Helpers.ObjectToVector(pupilEllipse.Value);
+                                    // Do stuff
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        // Do stuff
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
--- plugin/Scripts/Helpers.cs
+++ plugin/Scripts/Helpers.cs
@@ -15,5 +15,66 @@ namespace PupilLabs
             Debug.Log("Pupil version below 1 detected. V1 is required for 3D calibration");
             return false;
         }
+
+        private static object[] position_o;
+        public static Vector3 ObjectToVector (object source)
+        {
+            position_o = source as object[];
+            Vector3 result = Vector3.zero;
+            if (position_o.Length != 2 && position_o.Length != 3)
+                UnityEngine.Debug.Log ("Array length not supported");
+            else
+            {
+                result.x = (float)(double)position_o [0];
+                result.y = (float)(double)position_o [1];
+                if ( position_o.Length == 3)
+                    result.z = (float)(double)position_o [2];
+            }
+            return result;
+        }
+        private static Vector3 Position (object position, bool applyScaling)
+        {
+            Vector3 result = ObjectToVector (position);
+            if (applyScaling)
+                result /= PupilSettings.PupilUnitScalingFactor;
+            return result;
+        }
+
+        public static Vector3 VectorFromDictionary(Dictionary<string,object> source, string key)
+        {
+            if (source.ContainsKey (key))
+                return Position (source [key], false);
+            else
+                return Vector3.zero;
+        }
+        public static float FloatFromDictionary(Dictionary<string,object> source, string key)
+        {
+            object value_o;
+            source.TryGetValue (key, out value_o);
+            return (float)(double)value_o;
+        }
+        private static object IDo;
+        public static string StringFromDictionary(Dictionary<string,object> source, string key)
+        {
+            string result = "";
+            if (source.TryGetValue (key, out IDo))
+                result = IDo.ToString ();
+            return result;
+        }
+        public static Dictionary<object,object> DictionaryFromDictionary(Dictionary<string,object> source, string key)
+        {
+            if (source.ContainsKey(key))
+                return source[key] as Dictionary<object,object>;
+            else
+                return null;
+        }
+
+        public static string TopicsForDictionary(Dictionary<string,object> dictionary)
+        {
+            string topics = "";
+            foreach (var key in dictionary.Keys)
+                topics += key + ",";
+            return topics;
+        }
     }
 }
