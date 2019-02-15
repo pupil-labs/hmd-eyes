1af940d30ff09480bdd681e70156d3029b3a63ac
Felix Lange <langefx@gmail.com>
2019-02-15 16:59:44 +0100

    WIP Calibration Refactoring: better default settings

--- plugin/Scripts/CalibrationSettings.cs
+++ plugin/Scripts/CalibrationSettings.cs
@@ -17,38 +17,11 @@ namespace PupilLabs
 
         public string pluginName;
         public string positionKey;
-        public double[] ref_data;
         public float points = 5;
         public float markerScale = 0.03f;
-        public Vector2 centerPoint;
+        public Vector2 centerPoint = new Vector2(0.5f,0.5f);
         public Vector2[] vectorDepthRadius;
-        public int samplesPerDepth = 80;
+        public int samplesPerDepth = 40;
         public int samplesToIgnoreForEyeMovement = 10;
-
-        // public Type CalibrationType2D = new Type () 
-		// { 
-		// 	name = "2d",
-		// 	pluginName = "HMD_Calibration",
-		// 	positionKey = "norm_pos",
-		// 	ref_data = new double[]{ 0.0, 0.0 },
-		// 	points = 8,
-		// 	markerScale = 0.05f,
-		// 	centerPoint = new Vector2(0.5f,0.5f),
-		// 	vectorDepthRadius = new Vector2[] { new Vector2( 2f, 0.07f ) },
-		// 	samplesPerDepth = 120
-		// };
-
-		// public Type CalibrationType3D = new Type () 
-		// { 
-		// 	name = "3d",
-		// 	pluginName = "HMD_Calibration_3D",
-		// 	positionKey = "mm_pos",
-		// 	ref_data = new double[]{ 0.0, 0.0, 0.0 },
-		// 	points = 10,
-		// 	markerScale = 0.04f,
-		// 	centerPoint = new Vector2(0,-0.05f),
-		// 	vectorDepthRadius = new Vector2[] { new Vector2( 1f, 0.24f ) },
-		// 	samplesPerDepth = 40
-		// };
     }
 }
