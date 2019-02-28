19c540b804b926405b3bda196463a22120f16eb4
Felix Lange <langefx@gmail.com>
2019-02-28 11:33:30 +0100

    WIP: gaze listener and visualizer

---
+++ plugin/Scripts/GazeListener.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs
+{
+    public class GazeListener
+    {
+
+
+
+        private RequestController requestCtrl;
+        private SubscriptionsController subsCtrl;
+
+        public GazeListener(SubscriptionsController subsCtrl)
+        {
+            this.subsCtrl = subsCtrl;
+            this.requestCtrl = subsCtrl.requestCtrl;
+
+            requestCtrl.OnConnected += Enable;
+            requestCtrl.OnDisconnecting += Disable;
+
+            if (requestCtrl.IsConnected)
+            {
+                Enable();
+            }
+        }
+
+        ~GazeListener()
+        {
+            requestCtrl.OnConnected -= Enable;
+            requestCtrl.OnDisconnecting -= Disable;
+
+            if (requestCtrl.IsConnected)
+            {
+                Disable();
+            }
+        }
+
+        public void Enable()
+        {
+            Debug.Log("Enabling Gaze Listener");
+
+            subsCtrl.SubscribeTo("gaze.2d", Receive2DGaze);
+            subsCtrl.SubscribeTo("gaze.3d", Receive3DGaze);
+        }
+
+        public void Disable()
+        {
+            Debug.Log("Disabling Gaze Listener");
+
+            subsCtrl.UnsubscribeFrom("gaze.2d", Receive2DGaze);
+            subsCtrl.UnsubscribeFrom("gaze.3d", Receive3DGaze);
+        }
+
+        void Receive3DGaze(string topic, Dictionary<string, object> dictionary, byte[] thirdFrame = null)
+        {
+            Debug.Log($"3D Gaze msg topic: {topic}");
+
+
+        }
+
+        void Receive2DGaze(string topic, Dictionary<string, object> dictionary, byte[] thirdFrame = null)
+        {
+            Debug.Log($"2D Gaze msg topic: {topic}");
+
+
+        }
+    }
+}
---
+++ plugin/Scripts/GazeVisualizer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs
+{
+    public class GazeVisualizer : MonoBehaviour
+    {
+        public SubscriptionsController subscriptionsController;
+        public bool showGazeBeforeCalibration = false; //TODO
+        
+        [Range(0f,0.99f)]
+        public float confidenceThreshold = 0.6f;
+
+        GazeListener gazeListener = null;
+        
+
+        void OnEnable()
+        {
+            if (gazeListener == null)
+            {
+                gazeListener = new GazeListener(subscriptionsController);
+            }
+        }
+
+        void OnDisable()
+        {
+
+        }
+    }
+}
