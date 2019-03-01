0d4d69d4fe5ff6acd3e75c7db4a7b7346b3b77fc
Felix Lange <langefx@gmail.com>
2019-03-01 10:27:35 +0100

    simple utils script to disable gameobjects before calibration

---
+++ plugin/Scripts/DisableOnCalibrationStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs
+{
+	public class DisableOnCalibrationStart : MonoBehaviour {
+
+		public CalibrationController controller;
+
+		void OnEnable () 
+		{
+			controller.OnCalibrationStarted += DisableMePls;
+		}
+
+		void OnDisable ()
+		{
+			controller.OnCalibrationStarted -= DisableMePls;
+		}
+
+		void DisableMePls()
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
