2a73d72cff61a207791df3c19ebd4057ab304bd0
Felix Lange <langefx@gmail.com>
2019-03-05 10:56:19 +0100

    remove old plugin code and market scene demo

--- starter_project_vr/Assets/Market Scene Demo/Unity/Scripts/ScriptableObjects/ResettableScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-// This class is used to enforce a Reset function on ScriptableObjects.
-// It is used instead of an Interface as Interfaces do not serialize
-// and therefore can't be shown in the inspector.
-public abstract class ResettableScriptableObject : ScriptableObject
-{
-    public abstract void Reset ();
-}
