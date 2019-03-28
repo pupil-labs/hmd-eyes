e511cf27a4fbc34f234012379a7a39c0005825fc
Felix Lange <langefx@gmail.com>
2019-03-28 17:14:58 +0100

    new Demo showcasing scene switch with keeping the Pupil Connection alive

---
+++ plugin/Demos/SceneManagement/SetupOnSceneLoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace PupilLabs.Demos
+{
+    public class SetupOnSceneLoad : MonoBehaviour
+    {
+        public GazeVisualizer gazeVisualizer;
+
+        void Start(){
+            SubscriptionsController subsController = FindObjectOfType<SubscriptionsController>();
+
+            if (subsController == null)
+            {
+                Debug.LogWarning("No SubscriptionController found. Missing 'DontDestory' on Pupil Connection object?");
+                return;
+            }
+
+            gazeVisualizer.subscriptionsController = subsController;
+            gazeVisualizer.StartVisualizing();
+        }
+    }
+}
