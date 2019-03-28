349c5777d526f7be9f219efda625dd994d7c16cc
Felix Lange <langefx@gmail.com>
2019-03-28 16:41:53 +0100

    added small helper components for simple tasks

---
+++ plugin/Scripts/Helpers/LoadSceneAfterCalibration.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PupilLabs
+{
+    public class LoadSceneAfterCalibration : MonoBehaviour
+    {
+        public PupilLabs.CalibrationController calibrationController;
+
+        [Tooltip("Specify scene by name (needs to be added to 'BuildSettings/Scenes In Build'")]
+        public string sceneToLoad;
+
+        void OnEnable()
+        {
+            calibrationController.OnCalibrationSucceeded += LoadScene;
+        }
+
+        void OnDisable()
+        {
+            calibrationController.OnCalibrationSucceeded -= LoadScene;
+        }
+
+        void LoadScene()
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
