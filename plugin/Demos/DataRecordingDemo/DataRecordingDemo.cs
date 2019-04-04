5ae8db81c31ddad90b4783427a6f1e88d71eecaa
Felix Lange <langefx@gmail.com>
2019-04-04 17:40:58 +0200

    added first version of the recording controller + demo
    first version only includes start/stop recordings via pupil capture

---
+++ plugin/Demos/DataRecordingDemo/DataRecordingDemo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PupilLabs.Demos
+{
+    [RequireComponent(typeof(RecordingController))]
+    public class DataRecordingDemo : MonoBehaviour
+    {
+        public Text text;
+
+        RecordingController recording;
+
+        void Awake()
+        {
+            recording = GetComponent<RecordingController>();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (!recording.IsRecording)
+                {
+                    recording.StartRecording();
+                }
+                else
+                {
+                    recording.StopRecording();
+                }
+            }
+
+            bool connected = recording.requestCtrl.IsConnected;
+            text.text = connected ? "Connected" : "Not connected";
+
+            if (connected)
+            {
+                text.text += "\n\nPress R to Start/Stop the recording.";
+
+                var status = recording.IsRecording ? "recording" : "not recording";
+                text.text += $"\n\nStatus: {status}";
+            }
+        }
+    }
+}
---
+++ plugin/Scripts/RecordingController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs
+{
+    public class RecordingController : MonoBehaviour
+    {
+        public RequestController requestCtrl;
+
+        [SerializeField] private bool startRecordingButton;
+        [SerializeField] private bool stopRecordingButton;
+
+        public bool IsRecording { get; private set; }
+
+        void Update()
+        {
+            if (startRecordingButton)
+            {
+                startRecordingButton = false;
+                StartRecording();
+            }
+
+            if (stopRecordingButton)
+            {
+                stopRecordingButton = false;
+                StopRecording();
+            }
+        }
+
+        public void StartRecording()
+        {
+            if (requestCtrl == null)
+            {
+                Debug.LogWarning("Request Controller missing");
+                return;
+            }
+
+            if (!requestCtrl.IsConnected)
+            {
+                Debug.LogWarning("Not connected");
+                return;
+            }
+
+            if (IsRecording)
+            {
+                Debug.Log("Recording is already running.");
+                return;
+            }
+
+            var path = GetRecordingPath().Substring(2);
+            Debug.Log($"Recording path: {path}");
+
+            requestCtrl.Send(new Dictionary<string, object>
+            {
+                { "subject","recording.should_start" }
+                , { "session_name", path }
+                , { "record_eye",true}
+            });
+            IsRecording = true;
+        }
+
+        public void StopRecording()
+        {
+            if (requestCtrl == null)
+            {
+                Debug.LogWarning("Request Controller missing");
+                return;
+            }
+
+            if (!requestCtrl.IsConnected)
+            {
+                Debug.LogWarning("Not connected");
+                return;
+            }
+
+            if (!IsRecording)
+            {
+                Debug.Log("Recording is not running, nothing to stop.");
+                return;
+            }
+
+            requestCtrl.Send(new Dictionary<string, object> 
+            { 
+                { "subject", "recording.should_stop" } 
+            });
+
+            IsRecording = false;
+        }
+
+        private string GetRecordingPath()
+        {
+            string date = System.DateTime.Now.ToString ("yyyy_MM_dd");
+            string path = Application.dataPath + "/" + date;
+
+            path = path.Replace ("Assets/", ""); //go one folder up
+
+            if (!System.IO.Directory.Exists (path))
+                System.IO.Directory.CreateDirectory (path);
+
+            return path;
+        }    
+    }
+}
