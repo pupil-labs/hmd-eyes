769c822bfcb6cbfc60d74a6ea3de82e537d9cb33
Felix Lange <langefx@gmail.com>
2019-04-11 12:24:32 +0200

    WIP: demo for screencasting as frame.world

---
+++ plugin/Demos/ScreenCastDemo/ScreenCastDemo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCastDemo : MonoBehaviour
+{
+
+    public PupilLabs.ScreenCast screenCast;
+    public MeshRenderer renderer;
+
+    void OnEnable()
+    {
+        renderer.material.mainTexture = screenCast.streamTexture;
+    }
+}
---
+++ plugin/Scripts/ScreenCast.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NetMQ;
+using NetMQ.Sockets;
+using MessagePack;
+
+namespace PupilLabs
+{
+    public class ScreenCast : MonoBehaviour
+    {
+        public RequestController requestCtrl;
+        public Camera centerCam;
+        public int width, height;
+        public float delay = 0.3f;
+        
+        private RenderTexture renderTexture;
+        public Texture2D streamTexture;
+        bool isSetup = false;
+        PublisherSocket pubSocket;
+        int index = 0;
+        float tLastFrame;
+
+        void Awake()
+        {
+            renderTexture = new RenderTexture(width,height,24);
+            centerCam.targetTexture = renderTexture;
+            streamTexture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+        }
+
+        void OnEnable()
+        {
+            requestCtrl.OnConnected += Setup;
+        }
+
+        void Setup()
+        {
+            string connectionStr = requestCtrl.GetPubConnectionString();
+            pubSocket = new PublisherSocket(connectionStr);
+            // pubSocket.SendReady += ? 
+            
+            isSetup = true;
+            Debug.Log("screen cast ready");
+        }
+
+        void Update()
+        {
+            // activeCamera.Render();
+            RenderTexture.active = renderTexture;
+
+            // Read pixels
+            streamTexture.ReadPixels(new Rect(0,0,width,height),0,0);
+            streamTexture.Apply();
+
+            // Clean up
+            RenderTexture.active = null; // added to avoid errors 
+
+            if (!isSetup)
+            {
+                return;
+            }
+
+            // if (Time.time - tLastFrame > delay)
+            // {
+                Send();
+            //     tLastFrame = Time.time;
+            // }
+        }
+
+        void Send()
+        {
+            Debug.Log("Send");
+            byte[] rawTextureData = streamTexture.GetRawTextureData();
+
+            Dictionary<string, object> payload = new Dictionary<string, object> {
+                {"topic", "frame.world"},
+                {"width", width},
+                {"height", height},
+                {"index", index},
+                {"timestamp", Time.realtimeSinceStartup},
+                {"format", "rgb"},
+            };
+        
+            NetMQMessage m = new NetMQMessage();
+            m.Append("frame.world"); 
+            m.Append(MessagePackSerializer.Serialize<Dictionary<string, object>>(payload));
+            m.Append(rawTextureData);
+
+            pubSocket.SendMultipartMessage(m);
+
+            index++;
+        }
+    }
+}
