15a0ac4f301563aec640b5690c81013280b24a3e
Felix Lange <langefx@gmail.com>
2019-05-02 14:07:42 +0200

    wip: missing origin component

---
+++ plugin/Scripts/Origin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace PupilLabs
+{
+    public class Origin : MonoBehaviour
+    {
+        public Transform vrRigOrigin;
+
+        void Start()
+        {
+            if (vrRigOrigin == null)
+            {
+                Camera mainCamera = Camera.main;
+                if (mainCamera == null)
+                {
+                    Debug.LogError("No VR rig origin assinged and no main camera found.");
+                }
+                else
+                {
+                    vrRigOrigin = mainCamera.transform.parent;
+                }
+            }
+
+            transform.parent = vrRigOrigin;
+        }
+
+        void Update()
+        {
+            transform.localPosition = InputTracking.GetLocalPosition(XRNode.CenterEye);
+            transform.localRotation = InputTracking.GetLocalRotation(XRNode.CenterEye);
+        }
+    }
+}
