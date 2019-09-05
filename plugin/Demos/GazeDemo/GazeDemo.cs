689bb4bbcba3f1add5ad3b2a416f83ac017e3890
Felix Lange <langefx@gmail.com>
2019-09-05 13:16:24 +0200

    update gaze demo scene for the v1.0 release demo application

---
+++ plugin/Demos/GazeDemo/GazeDemo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GazeDemo : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
