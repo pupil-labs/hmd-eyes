3c1a85e9e0874fb6a7b7168f91ab5c2e4fd6e348
Felix Lange <langefx@gmail.com>
2019-09-27 15:10:02 +0200

    gaze ray demo update: mirror mode

--- plugin/Demos/GazeRaysDemo/GazeRaysVis.cs
+++ plugin/Demos/GazeRaysDemo/GazeRaysVis.cs
@@ -6,28 +6,38 @@ namespace PupilLabs.Demos
     public class GazeRay
     {
         public LineRenderer gazeLine;
-        public Transform eye;
+        public Transform fixedEyePos;
 
-        public void Update(Transform origin, Vector3 eyePos, Vector3 dir, float rayLength, bool flipX)
+        public void Update(Transform origin, Vector3 eyePos, Vector3 dir, float rayLength, bool useFixedEyePos)
         {
             dir.Normalize();
-            if (flipX)
-            {
-                dir.x *= -1f;
-            }
+            dir.x *= -1f;
             dir *= rayLength;
             dir = origin.TransformDirection(dir);
-            eyePos = origin.TransformPoint(eyePos);
-
-            if (gazeLine != null && eye != null)
+            
+            if (fixedEyePos != null && useFixedEyePos)
             {
-                eye.position = eyePos;
-
-                gazeLine.SetPosition(0, eye.position);
-                gazeLine.SetPosition(1, eye.position + dir);
-
-                eye.LookAt(eye.position + dir);
+                eyePos = fixedEyePos.position;
             }
+            else
+            {
+                eyePos = origin.TransformPoint(eyePos);
+            }
+            
+
+            if (gazeLine != null)
+            {
+                gazeLine.SetPosition(0, eyePos);
+                gazeLine.SetPosition(1, eyePos + dir);
+            }
+        }
+
+        public void VisualizeConfidence(float confidence)
+        {
+            
+            Color c = gazeLine.startColor;
+            c.a = confidence;
+            gazeLine.startColor = gazeLine.endColor = c;
         }
     }
 
@@ -38,19 +48,18 @@ namespace PupilLabs.Demos
         public CalibrationController calibrationController;
         public float confidenceThreshold = 0.6f;
         [Header("Origin")]
-        public Transform firstPersonOrigin;
-        public Transform thirdPersonOrigin;
-        public bool useFirstPersonOrigin = true;
+        public Transform origin;
+        public bool useFixedEyePos = true;
         [Header("Gaze Rays")]
         public GazeRay leftGaze;
         public GazeRay rightGaze;
         [Range(0.1f, 20)]
-        public float rayLength = 5; //TODO ray!
+        public float rayLength = 5;
 
         bool isGazing = false;
         Vector3 gazeNormalLeft, gazeNormalRight;
         Vector3 eyeCenterLeft, eyeCenterRight;
-        Transform origin;
+        float confidenceLeft, confidenceRight;
 
         void OnEnable()
         {
@@ -65,55 +74,53 @@ namespace PupilLabs.Demos
 
         void Update()
         {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                useFixedEyePos = !useFixedEyePos;
+            }
+
             if (!isGazing)
             {
                 return;
             }
 
-            //switch origin
-            if (Input.GetKeyDown(KeyCode.T))
-            {
-                useFirstPersonOrigin = !useFirstPersonOrigin;
-            }
-
-            if (useFirstPersonOrigin && origin != firstPersonOrigin)
-            {
-                origin = firstPersonOrigin;
-            }
-            else if (!useFirstPersonOrigin && origin != thirdPersonOrigin)
-            {
-                origin = thirdPersonOrigin;
-            }
-
             //update rays
             if (leftGaze != null)
             {   
-                leftGaze.Update(origin, eyeCenterLeft, gazeNormalLeft, rayLength, false);
+                leftGaze.VisualizeConfidence(confidenceLeft);
+                leftGaze.Update(origin, eyeCenterLeft, gazeNormalLeft, rayLength, useFixedEyePos);
             }
 
             if (rightGaze != null)
             {
-                rightGaze.Update(origin, eyeCenterRight, gazeNormalRight, rayLength, false);
+                rightGaze.VisualizeConfidence(confidenceRight);
+                rightGaze.Update(origin, eyeCenterRight, gazeNormalRight, rayLength, useFixedEyePos);
             }
         }
 
         void ReceiveEyeData(GazeData data)
         {
-            if (data.Confidence < confidenceThreshold)
+
+            bool valid = data.Confidence > confidenceThreshold;
+
+            if (data.IsEyeDataAvailable(1))
             {
-                return;
+                if (valid)
+                {
+                    gazeNormalLeft = data.GetGazeNormal(1);
+                    eyeCenterLeft = data.GetEyeCenter(1);
+                }
+                confidenceLeft = data.Confidence;
             }
 
             if (data.IsEyeDataAvailable(0))
             {
-                gazeNormalLeft = data.GazeNormal0;
-                eyeCenterLeft = data.EyeCenter0;
-            }
-
-            if (data.IsEyeDataAvailable(1))
-            {
-                gazeNormalRight = data.GazeNormal1;
-                eyeCenterRight = data.EyeCenter1;
+                if (valid)
+                {
+                    gazeNormalRight = data.GetGazeNormal(0);
+                    eyeCenterRight = data.GetEyeCenter(0);
+                }
+                confidenceRight = data.Confidence;
             }
         }
     }
---
+++ plugin/Demos/GazeRaysDemo/MirrorHead.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace PupilLabs.Demos
+{
+    public class MirrorHead : MonoBehaviour
+    {
+        public Transform target;
+        public new Transform transform;
+
+        void Update()
+        {
+            Vector3 pos = transform.parent.position;
+            pos.y = target.position.y - transform.localPosition.y; 
+            transform.parent.position = pos;
+
+            transform.LookAt(target);
+            
+            Vector3 targetDirection = target.TransformDirection(Vector3.forward);
+            Vector3 targetOffset = transform.position-target.position;
+            
+            float angleY = Vector3.SignedAngle(Vector3.ProjectOnPlane(targetOffset,Vector3.up), Vector3.ProjectOnPlane(targetDirection,Vector3.up), Vector3.up);
+            float angleX = Vector3.SignedAngle(Vector3.ProjectOnPlane(targetOffset,Vector3.forward), Vector3.ProjectOnPlane(targetDirection,Vector3.forward), Vector3.back);
+            
+            transform.Rotate(new Vector3(angleX,-angleY,0));
+        }
+    }
+}
