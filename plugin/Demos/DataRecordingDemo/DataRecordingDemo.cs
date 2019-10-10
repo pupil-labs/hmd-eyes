ff09a5c42657acc00fe926ee5524acf8ccd589d7
eioe <klotzsche@cbs.mpg.de>
2019-10-10 10:10:18 +0200

    delete old commands

--- plugin/Demos/DataRecordingDemo/DataRecordingDemo.cs
+++ plugin/Demos/DataRecordingDemo/DataRecordingDemo.cs
@@ -9,7 +9,6 @@ namespace PupilLabs.Demos
     public class DataRecordingDemo : MonoBehaviour
     {
         public Text text;
-        public string TriggerLabel = "custom_annotation_label";
 
         RecordingController recording;
 
@@ -31,13 +30,6 @@ namespace PupilLabs.Demos
                     recording.StopRecording();
                 }
             }
-            // ***************************************************************************
-            if (Input.GetKeyDown(KeyCode.T))
-            {
-                recording.SendSimpleTrigger(TriggerLabel.ToString(), 0f);
-            }
-            // ---------------------------------------------------------------------------
-
 
             bool connected = recording.requestCtrl.IsConnected;
             text.text = connected ? "Connected" : "Not connected";
@@ -48,10 +40,7 @@ namespace PupilLabs.Demos
 
                 var status = recording.IsRecording ? "recording" : "not recording";
                 text.text += $"\n\nStatus: {status}";
-                // ***************************************************************************
-                text.text += "\n\nPress T to send trigger.";
-                // ---------------------------------------------------------------------------
-    }
-}
+            }
+        }
     }
 }
