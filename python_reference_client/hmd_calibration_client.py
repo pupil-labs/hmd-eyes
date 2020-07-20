fe910909a6506b5fa09a401c7d6db974b96a6114
Patrick Faion <pfa@pupil-labs.com>
2020-07-20 16:56:33 +0200

    Adjust hmd_calibration_client for Pupil v2.0

--- python_reference_client/hmd_calibration_client.py
+++ python_reference_client/hmd_calibration_client.py
@@ -35,33 +35,44 @@ time.sleep(2)
 
 
 # set calibration method to hmd calibration
-n = {"subject": "start_plugin", "name": "HMD_Calibration", "args": {}}
+n = {"subject": "start_plugin", "name": "HMD3DChoreographyPlugin", "args": {}}
 print(send_recv_notification(n))
 
 # start caliration routine with params. This will make pupil start sampeling pupil data.
+# the eye-translations have to be in mm
 n = {
     "subject": "calibration.should_start",
-    "hmd_video_frame_size": (1000, 1000),
-    "outlier_threshold": 35,
+    "translation_eye0": [27.84765, 0.0, 0.0],
+    "translation_eye1": [-27.84765, 0.0, 0.0],
+    "record": True,
 }
 print(send_recv_notification(n))
 
 
 # Mockup logic for sample movement:
-# We sample some reference positions (in normalized screen coords).
+# We sample some reference positions in scene coordinates (mm) relative to the HMD.
 # Positions can be freely defined
 
 ref_data = []
 for pos in (
-    (0.5, 0.5),
-    (0, 0),
-    (0, 0.5),
-    (0, 1),
-    (0.5, 1),
-    (1, 1),
-    (1, 0.5),
-    (1, 0),
-    (0.5, 0),
+    (0.0, 0.0, 600.0),
+    (0.0, 0.0, 1000.0),
+    (0.0, 0.0, 2000.0),
+    (180.0, 0.0, 600.0),
+    (240.0, 0.0, 1000.0),
+    (420.0, 0.0, 2000.0),
+    (55.62306, 195.383, 600.0),
+    (74.16407, 260.5106, 1000.0),
+    (129.7871, 455.8936, 2000.0),
+    (-145.6231, 120.7533, 600.0),
+    (-194.1641, 161.0044, 1000.0),
+    (-339.7872, 281.7577, 2000.0),
+    (-145.6231, -120.7533, 600.0),
+    (-194.1641, -161.0044, 1000.0),
+    (-339.7872, -281.7577, 2000.0),
+    (55.62306, -195.383, 600.0),
+    (74.16407, -260.5106, 1000.0),
+    (129.7871, -455.8936, 2000.0),
 ):
     print("subject now looks at position:", pos)
     for s in range(60):
@@ -72,8 +83,8 @@ for pos in (
         t = get_pupil_timestamp()
 
         # in this mockup  the left and right screen marker positions are identical.
-        datum0 = {"norm_pos": pos, "timestamp": t, "id": 0}
-        datum1 = {"norm_pos": pos, "timestamp": t, "id": 1}
+        datum0 = {"mm_pos": pos, "timestamp": t}
+        datum1 = {"mm_pos": pos, "timestamp": t}
         ref_data.append(datum0)
         ref_data.append(datum1)
         time.sleep(1 / 60.0)  # simulate animation speed.
@@ -82,13 +93,20 @@ for pos in (
 # Send ref data to Pupil Capture/Service:
 # This notification can be sent once at the end or multiple times.
 # During one calibraiton all new data will be appended.
-n = {"subject": "calibration.add_ref_data", "ref_data": ref_data}
+n = {
+    "subject": "calibration.add_ref_data",
+    "ref_data": ref_data,
+    "record": True,
+}
 print(send_recv_notification(n))
 
 # stop calibration
 # Pupil will correlate pupil and ref data based on timestamps,
 # compute the gaze mapping params, and start a new gaze mapper.
-n = {"subject": "calibration.should_stop"}
+n = {
+    "subject": "calibration.should_stop",
+    "record": True,
+}
 print(send_recv_notification(n))
 
 time.sleep(2)
