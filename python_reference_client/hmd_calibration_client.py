c351c3df875be891fda5c44b5b6b47a93f0171d8
Patrick Faion <pfa@pupil-labs.com>
2020-07-20 15:28:04 +0200

    Apply black formatter on python files

--- python_reference_client/hmd_calibration_client.py
+++ python_reference_client/hmd_calibration_client.py
@@ -1,42 +1,49 @@
-'''
+"""
 HMD calibration client example.
 This script shows how to talk to Pupil Capture or Pupil Service
 and run a gaze mapper calibration.
-'''
+"""
 import zmq, msgpack, time
+
 ctx = zmq.Context()
 
 
-#create a zmq REQ socket to talk to Pupil Service/Capture
+# create a zmq REQ socket to talk to Pupil Service/Capture
 req = ctx.socket(zmq.REQ)
-req.connect('tcp://127.0.0.1:50020')
+req.connect("tcp://127.0.0.1:50020")
 
 
-#convenience functions
+# convenience functions
 def send_recv_notification(n):
     # REQ REP requirese lock step communication with multipart msg (topic,msgpack_encoded dict)
-    req.send_string('notify.%s'%n['subject'], flags=zmq.SNDMORE)
+    req.send_string("notify.%s" % n["subject"], flags=zmq.SNDMORE)
     req.send(msgpack.dumps(n, use_bin_type=True))
     return req.recv_string()
 
+
 def get_pupil_timestamp():
-    req.send_string('t') #see Pupil Remote Plugin for details
+    req.send_string("t")  # see Pupil Remote Plugin for details
     return float(req.recv_string())
 
+
 # set start eye windows
-n = {'subject':'eye_process.should_start.0','eye_id':0, 'args':{}}
+n = {"subject": "eye_process.should_start.0", "eye_id": 0, "args": {}}
 print(send_recv_notification(n))
-n = {'subject':'eye_process.should_start.1','eye_id':1, 'args':{}}
+n = {"subject": "eye_process.should_start.1", "eye_id": 1, "args": {}}
 print(send_recv_notification(n))
 time.sleep(2)
 
 
 # set calibration method to hmd calibration
-n = {'subject':'start_plugin','name':'HMD_Calibration', 'args':{}}
+n = {"subject": "start_plugin", "name": "HMD_Calibration", "args": {}}
 print(send_recv_notification(n))
 
 # start caliration routine with params. This will make pupil start sampeling pupil data.
-n = {'subject':'calibration.should_start', 'hmd_video_frame_size':(1000,1000), 'outlier_threshold':35}
+n = {
+    "subject": "calibration.should_start",
+    "hmd_video_frame_size": (1000, 1000),
+    "outlier_threshold": 35,
+}
 print(send_recv_notification(n))
 
 
@@ -45,8 +52,18 @@ print(send_recv_notification(n))
 # Positions can be freely defined
 
 ref_data = []
-for pos in ((0.5,0.5),(0,0),(0,0.5),(0,1),(0.5,1),(1,1),(1,0.5),(1,0),(.5,0)):
-    print('subject now looks at position:', pos)
+for pos in (
+    (0.5, 0.5),
+    (0, 0),
+    (0, 0.5),
+    (0, 1),
+    (0.5, 1),
+    (1, 1),
+    (1, 0.5),
+    (1, 0),
+    (0.5, 0),
+):
+    print("subject now looks at position:", pos)
     for s in range(60):
         # you direct screen animation instructions here
 
@@ -55,27 +72,24 @@ for pos in ((0.5,0.5),(0,0),(0,0.5),(0,1),(0.5,1),(1,1),(1,0.5),(1,0),(.5,0)):
         t = get_pupil_timestamp()
 
         # in this mockup  the left and right screen marker positions are identical.
-        datum0 = {'norm_pos':pos,'timestamp':t,'id':0}
-        datum1 = {'norm_pos':pos,'timestamp':t,'id':1}
+        datum0 = {"norm_pos": pos, "timestamp": t, "id": 0}
+        datum1 = {"norm_pos": pos, "timestamp": t, "id": 1}
         ref_data.append(datum0)
         ref_data.append(datum1)
-        time.sleep(1/60.) #simulate animation speed.
+        time.sleep(1 / 60.0)  # simulate animation speed.
 
 
 # Send ref data to Pupil Capture/Service:
 # This notification can be sent once at the end or multiple times.
 # During one calibraiton all new data will be appended.
-n = {'subject':'calibration.add_ref_data','ref_data':ref_data}
+n = {"subject": "calibration.add_ref_data", "ref_data": ref_data}
 print(send_recv_notification(n))
 
 # stop calibration
 # Pupil will correlate pupil and ref data based on timestamps,
 # compute the gaze mapping params, and start a new gaze mapper.
-n = {'subject':'calibration.should_stop'}
+n = {"subject": "calibration.should_stop"}
 print(send_recv_notification(n))
 
 time.sleep(2)
-
-
-
 
--- python_reference_client/logging_monitor.py
+++ python_reference_client/logging_monitor.py
@@ -3,13 +3,13 @@ from zmq_tools import *
 
 ctx = zmq.Context()
 requester = ctx.socket(zmq.REQ)
-requester.connect('tcp://127.0.0.1:50020')
+requester.connect("tcp://127.0.0.1:50020")
 
-requester.send_string('SUB_PORT')
+requester.send_string("SUB_PORT")
 ipc_sub_port = requester.recv_string()
-monitor = Msg_Receiver(ctx,'tcp://127.0.0.1:%s'%ipc_sub_port,topics=('logging.',))
+monitor = Msg_Receiver(ctx, "tcp://127.0.0.1:%s" % ipc_sub_port, topics=("logging.",))
 
 while True:
-    topic,l = monitor.recv()
-    print('%s: %s - %s'%(l['name'],l['levelname'],l['msg']))
+    topic, l = monitor.recv()
+    print("%s: %s - %s" % (l["name"], l["levelname"], l["msg"]))
 
